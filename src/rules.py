ddbcd705e03fe46cd74d86cd91f12d98e45ee5c8
Miguel de la Rosa <miguelanroga@gmail.com>
2021-07-07 19:03:50 -0500

    ADD: Serialize output

--- src/rules.py
+++ src/rules.py
@@ -87,7 +87,6 @@ class Team:
 		for player in self._players:
 			player_bonus = (team_bonus + player.goles / Level[player.nivel].value) / 2
 			player.sueldo_completo = round(player.sueldo + player.bono * player_bonus, 2)
-			print(player)
 
 	def player_add(self, player:Player) -> None:
 		"""
@@ -137,30 +136,45 @@ def calculate(environ:dict, service:object) -> None:
 		data = environ['wsgi.input'].read(data_size)
 		data = json.loads(data)
 	except:
-	# If data can't be read then return error as response
+	# If data can't be read then return error as response.
 		service.status = common.HTTPS.HTTPS_400
 		service.response.append(repr(common.HTTPS.HTTPS_400.value))
 		return
 
 
-	try:
-		for chunk in data['jugadores']:
-		# Test automated deserialize
-			player = Player(**chunk)
+	#try:
+	for chunk in data['jugadores']:
+	# Test automated deserialize
+		player = Player(**chunk)
 
-			# Separate players by team
-			if player.equipo not in teams:
+		# Separate players by team
+		if player.equipo not in teams:
+			try:
 				teams[player.equipo] = Team()
+			except:
+			# If player object can't be created the JSON is bad formed.
+				service.status = common.HTTPS.HTTPS_400
+				service.response.append(repr(common.HTTPS.HTTPS_400.value))
+				return
 
-			teams[player.equipo].player_add(player)
 
-		# Calculate wages
-		for key, team in teams.items():
-			team.calculate()
+		teams[player.equipo].player_add(player)
 
-		# Return a copy of request data
-		service.response.append(str(data))
-	except:
-		# Server error
-		service.status = common.HTTPS.HTTPS_500
-		service.response.append(repr(common.HTTPS.HTTPS_500.value))
+	# Calculate wages
+	for key, team in teams.items():
+		team.calculate()
+
+	# Serialize output
+	output = []
+
+	for key, team in teams.items():
+		for player in team._players:
+			output.append(player.__dict__)
+
+	service.response.append('{"jugadores": ')
+	service.response.append(json.dumps(output))
+	service.response.append('}')
+	#except:
+		## Server error
+		#service.status = common.HTTPS.HTTPS_500
+		#service.response.append(repr(common.HTTPS.HTTPS_500.value))
